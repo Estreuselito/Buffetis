982c52b8a108ffd3743db2afa586c8ec8930fc08
Yannik Suhre <yannik@familie-suhre.de>
2021-02-19 09:13:27 +0100

    Checking Sec

--- Python/auth.py
+++ Python/auth.py
@@ -1,6 +1,6 @@
 # This script authenticates yourself against the AWS instance
-AWS_USERNAME = ""
-AWS_PASSWORD = ""
+AWS_USERNAME = "A"
+AWS_PASSWORD = "2"
 
 if not AWS_USERNAME and not AWS_PASSWORD:
     AWS_USERNAME = "Public"
