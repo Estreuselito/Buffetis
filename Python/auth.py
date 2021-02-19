ae3c71b059f8c29ed810020f68aae6297f3503a2
Yannik Suhre <yannik@familie-suhre.de>
2021-02-19 10:10:19 +0100

    test

--- Python/auth.py
+++ Python/auth.py
@@ -1,6 +1,6 @@
 # This script authenticates yourself against the AWS instance
-AWS_USERNAME = "Abc"
-AWS_PASSWORD = "224"
+AWS_USERNAME = "Abcd"
+AWS_PASSWORD = "224e"
 
 if not AWS_USERNAME and not AWS_PASSWORD:
     AWS_USERNAME = "Public"
