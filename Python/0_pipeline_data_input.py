2476e603e6badb6767b34a3a7a0c951c1cadad6f
Yannik Suhre <yannik@familie-suhre.de>
2021-02-25 10:43:52 +0100

    inserted faulty code

--- Python/0_pipeline_data_input.py
+++ Python/0_pipeline_data_input.py
@@ -2,3 +2,10 @@
 # into the database.
 import pandas as pd
 from data_storage import connection
+
+berkshire = pd.read_csv("./data/berkshire_investments.csv",
+                        skiprows=1, encoding="utf-8")
+
+berkshire.to_sql("investments_buffet",
+                 connection=connection,
+                 if_exists="replace")
